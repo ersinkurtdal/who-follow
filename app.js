d2223d9643b926f58be18de6104c14dd5b33194c
Ersin Kurtdal <ersinkurtdal@gmail.com>
2019-01-01 15:57:47 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,111 @@
+const fs = require('fs'); // https://nodejs.org/api/fs.html
+
+const Twitter = require('twitter'); // https://github.com/desmondmorris/node-twitter
+
+const config = {
+	// application keys https://developer.twitter.com/en/apps
+	keys:{
+		consumer_key: '',
+		consumer_secret: '',
+		access_token_key: '',
+		access_token_secret: '',
+	},
+	screen_name: '',
+	notFriendsOutput: `${__dirname}/not-friends.json`,
+	notFollowersOutput: `${__dirname}/not-followers.json`
+};
+
+const client = new Twitter(config.keys);
+
+/**	
+ * Twitter api query
+ *
+ * @param {string} queryString https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference
+ * @param {object} params
+ *
+ * @return {object}
+ */ 
+const query = (queryString, params) => {
+	return new Promise((resolve, reject) => {
+		client.get(queryString, params, function(error, data, response) {
+			if (error) return reject(error);
+			resolve(data);
+		});
+	});
+};
+
+/**	
+ * Calls query function by next_cursor
+ * 
+ * @param {string} queryString https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference
+ * @param {object} params
+ * @param {string} id = null
+ *
+ * @return {array} user ids array
+ */ 
+const getUserData = async (queryString, params, id = null) => {
+
+	const data = [];
+	let users = [];
+
+	do{
+		try{
+			users = await query(queryString, params);
+			data.push(...(id ? users[id] : users));
+			params.cursor = users.next_cursor;
+		}catch(e){
+			console.error(e);
+			break;
+		}
+	}while(typeof users.next_cursor !== 'undefined' && users.next_cursor);
+
+	return data;
+};
+
+/**	
+ * Gets user screen name and profile image url by Twitter ID
+ * And writes it to output file in json format
+ *
+ * @param {array} users A comma separated list of user ids, up to 100 are allowed in a single request.
+ * @param {string} output
+ * 
+ * @return void
+ */ 
+const getUsersInfo = async (users, output) => {
+
+	const data = [];
+		size = users.length;
+
+	try{
+		for (let i = 0; i < size; i+=100) {
+			let slices = users.slice(i, i+100),
+				result = await query('users/lookup', {user_id: slices.join()}),
+				report = result.map(user => {
+					return {screen_name: user.screen_name, profile_image: user.profile_image_url_https};
+				});
+			data.push(...report);
+		}
+
+		fs.writeFileSync(output, JSON.stringify(data, null, 4));
+		console.info(`${data.length} user${data.length > 1 ? 's' : ''} information saved in ${output}`);
+	}catch(e){
+		console.error(e);
+	}
+};
+
+// the main function
+(async () => {
+
+	const params = {screen_name: config.screen_name};
+
+	// get followers and friends IDs
+	let followers = await getUserData('followers/ids', params, 'ids');
+	let friends = await getUserData('friends/ids', params, 'ids');
+
+	// gets user data by ID's that according to filters and saves it the output file
+	if(followers && followers.length && friends && friends.length){
+		await getUsersInfo(followers.filter(id => !friends.includes(id)), config.notFriendsOutput);
+		await getUsersInfo(friends.filter(id => !followers.includes(id)), config.notFollowersOutput);
+	}
+
+})();
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Twitter Who Follow App
+
+> This app creates a list of users who do not follow the current user back on Twitter. You can also see a list of users who are following the current user but the user haven't followed them yet.
+
+Node.js must be installed. [https://nodejs.org/en/](https://nodejs.org/en/)
+
+This app only uses [Twitter for Node.js](https://www.npmjs.com/package/twitter) library. You can install it with this command in terminal.
+
+	npm install
+
+For create a Twitter app please visit. [https://developer.twitter.com/en/apps](https://developer.twitter.com/en/apps)
+
+You can find Consumer API keys and Access token & access token secret keys on **Keys and tokens** tab. These keys must be placed in the config object in app.js.
+
+The **screen_name** in config array must be a Twitter username.
+
+Output files paths are notFriendsOutput and notFollowersOutput in config object.
+
+Runs the app:
+
+	node app.js
+
+This app doesn't control **x-rate-limit-remaining** in response codes at every query. Because in most cases the queries are completed within the boundaries.
+
+If you are getting "message": "Rate limit exceeded" error please check the Twitter Api documentation.
+[Twitter Rate Limiting](https://developer.twitter.com/en/docs/basics/rate-limiting.html)
+
+### License ###
+The plugin is available under the [MIT license](http://opensource.org/licenses/MIT).
